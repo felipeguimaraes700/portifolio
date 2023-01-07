73ebf0afd7ba8278865c121e3de1ece028052bd7
Felipe Guimarães <felipe700guima@gmail.com>
2023-01-07 18:21:55 -0300

    colocando intereção no  menu

---
+++ js/script.js
@@ -0,0 +1,13 @@
+
+
+function openMenu(){
+    document.getElementById('container-menu').style.width='270px';
+    document.getElementById('main-container').style.marginLeft='270px';
+    document.getElementById('icon-menu').style.display='none';
+}
+function closeMenu(){
+    document.getElementById('container-menu').style.width='0px';
+    document.getElementById('main-container').style.marginLeft='0px';
+    document.getElementById('icon-menu').style.display='flex';
+}
+
